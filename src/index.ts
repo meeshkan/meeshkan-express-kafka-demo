ce34ec6d6c079bc0faaa53dde590a46e23019771
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-02-26 11:30:11 +0200

    Add user stuff.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express = require("express");
 import debug = require("debug");
 import httpExpress from "@meeshkanml/express-middleware";
 import * as kafka from "@meeshkanml/http-types-kafka";
+import { v4 as uuidv4 } from "uuid";
 import { HttpExchange } from "http-types";
 
 const debugLog = debug("express-app");
@@ -12,8 +13,78 @@ const KAFKA_CONFIG: kafka.KafkaConfig = {
 };
 const PORT = 3000;
 
+// Resource representation
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface CreateUserInput {
+  name: string;
+  email: string;
+}
+
+class UserStore {
+  private readonly users: Record<string, User> = {};
+  constructor() {}
+
+  getUserById(userId: string): User | undefined {
+    return this.users[userId];
+  }
+
+  createUser(userInput: CreateUserInput): User {
+    const userId = uuidv4();
+    const user: User = {
+      id: userId,
+      name: userInput.name,
+      email: userInput.email,
+    };
+    this.users[userId] = user;
+    return user;
+  }
+}
+
+const parseCreateUserInput = (body: any): CreateUserInput => {
+  return {
+    name: body.name,
+    email: body.email,
+  };
+};
+
+const usersRouter = (userStore: UserStore): express.Router => {
+  const router = express.Router();
+
+  router.post("/", (req: express.Request, res: express.Response) => {
+    // Handle post user
+    let userInput: CreateUserInput;
+    try {
+      userInput = parseCreateUserInput(req.body);
+    } catch (err) {
+      return res.sendStatus(404);
+    }
+    const newUser = userStore.createUser(userInput);
+    return res.json(newUser);
+  });
+
+  router.get("/:userId", (req: express.Request, res: express.Response) => {
+    // Handle get user
+    const userId = req.params.userId;
+    const maybeUser = userStore.getUserById(userId);
+    if (maybeUser) {
+      return res.json(maybeUser);
+    } else {
+      return res.sendStatus(404);
+    }
+  });
+
+  return router;
+};
+
 const buildApp = (kafkaTransport: kafka.HttpTypesKafkaProducer) => {
   const app = express();
+
+  app.use(express.json());
 
   const transport = async (exchange: HttpExchange) => {
     debugLog("Sending an exchange to Kafka");
@@ -26,9 +97,9 @@ const buildApp = (kafkaTransport: kafka.HttpTypesKafkaProducer) => {
     })
   );
 
-  app.get("/", (_: express.Request, res: express.Response) => {
-    res.json({ hello: "world" });
-  });
+  const userStore = new UserStore();
+
+  app.use("/users", usersRouter(userStore));
 
   return app;
 };
